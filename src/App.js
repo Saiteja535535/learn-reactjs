64fe0bd2242268edd635f8bfda6666439abe105e
Saiteja535535 <jella21498@gmail.com>
2025-04-01 23:15:16 +0530

    kept to default name in app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React JS Test Done
+          Learn React
         </a>
       </header>
     </div>
